d2c61b6732cba317d0f9376f29ee50c1c25bdbc3
Shaun <swsorour8@gmail.com>
2024-10-01 15:07:07 +0200

    formatting

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,12 +24,10 @@ export default defineConfig({
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-
     {
       name: 'Microsoft Edge',
       use: { ...devices['Desktop Edge'], channel: 'msedge' },
