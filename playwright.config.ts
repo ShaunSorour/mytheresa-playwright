17a8032bc92a51cfb985607b6362bf63b72b696d
Shaun <swsorour8@gmail.com>
2025-07-11 12:12:01 +0200

    results output folder change

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   globalSetup: require.resolve('./setup/global-setup'),
   globalTimeout: 60 * 60 * 1000,
   testDir: './tests/',
+  outputDir: './results/artifacts/',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
