9255753dc4d6d148c42b86ba33b3b95630e6ee58
Shaun <swsorour8@gmail.com>
2025-07-21 13:51:58 +0200

    structure changes

--- tests/api/movie/movie.spec.ts
+++ tests/api/movie/movie.spec.ts
@@ -1,6 +1,7 @@
 import { expect, test } from '@playwright/test';
 import { FavouriteMovieRequest, FavouriteMovieResponse } from '../../../types/movie/favourite';
 import { Movie_APIHelper } from '../../../utilities/api/movie/movie-api-helper';
+import { PopularMoviesResponse } from '../../../types/movie/movie';
 
 
 let movieAPI: Movie_APIHelper;
@@ -9,18 +10,50 @@ test.beforeEach(async ({ request }) => {
     movieAPI = new Movie_APIHelper(request);
 });
 
-test('Add fave movie', async () => {
+test('Get Popular Movies', async () => {
+    const language = 'en-US';
+    const page = '1';
 
+    const response = await movieAPI.getPopularMovies(language, page);
+    const popularMovies: PopularMoviesResponse = await response.json();
+
+    expect(response.status()).toBe(200);
+    expect(response.status()).toBeTruthy();
+    expect(popularMovies).toHaveProperty('page');
+    expect(popularMovies).toHaveProperty('results');
+});
+
+test('Add Fave Movie', async () => {
     const movie: FavouriteMovieRequest = {
         media_type: 'movie',
         media_id: 550,
         favorite: true,
     };
 
-    const faveMovie: FavouriteMovieResponse = await movieAPI.addFavouriteMovie(movie);
+    const response = await movieAPI.addFavouriteMovie(movie);
+    const faveMovie: FavouriteMovieResponse = await response.json();
 
+    expect(response.status()).toBe(201);
+    expect(response.status()).toBeTruthy();
     expect(faveMovie).toHaveProperty('success', true);
     expect(faveMovie).toHaveProperty('status_message', 'The item/record was updated successfully.');
     expect(faveMovie).toHaveProperty('status_code', 12);
 });
 
+test('Add Fave Movie - NEGATIVE', async () => {
+    const movie: FavouriteMovieRequest = {
+        media_type: 'INVALID',
+        media_id: 550,
+        favorite: true,
+    };
+
+    const response = await movieAPI.addFavouriteMovie(movie);
+    const faveMovie: FavouriteMovieResponse = await response.json();
+
+    console.log(faveMovie)
+    expect(response.status()).toBe(201);
+    expect(response.status()).toBeTruthy();
+    expect(faveMovie).toHaveProperty('success', true);
+    expect(faveMovie).toHaveProperty('status_message', 'The item/record was updated successfully.');
+    expect(faveMovie).toHaveProperty('status_code', 12);
+});
--- utilities/api/movie/movie-api-helper.ts
+++ utilities/api/movie/movie-api-helper.ts
@@ -1,5 +1,4 @@
-import { PopularMoviesResponse } from './../../../types/movie/movie';
-import { APIRequestContext, expect } from '@playwright/test';
+import { APIRequestContext, expect, APIResponse } from '@playwright/test';
 import { FavouriteMovieRequest, FavouriteMovieResponse } from '../../../types/movie/favourite';
 import { BaseAPIHelper } from './base-api-helper';
 
@@ -12,23 +11,23 @@ export class Movie_APIHelper extends BaseAPIHelper {
         this.movieAccountID = process.env.MOVIE_ACCOUNT_ID || '';
     }
 
-    async getPopularMovies(language: string, page: string): Promise<PopularMoviesResponse> {
-        const response = await this.request.get(`${this.baseUrl}/movie/popular?language=${language}&page=${page}`);
-        expect(response.status()).toBe(200);
-        const movies: PopularMoviesResponse = await response.json();
-        return movies;
+    async getPopularMovies(language: string, page: string): Promise<APIResponse> {
+        const response = await this.request.get(`${this.baseUrl}/movie/popular?language=${language}&page=${page}`,
+            {
+                headers: this.getAuthHeaders()
+            });
+
+        return response;
     }
 
-    async addFavouriteMovie(favourite: FavouriteMovieRequest): Promise<FavouriteMovieResponse> {
-        const response = await this.request.post(
-            `${this.baseUrl}/account/${this.movieAccountID}/favorite`,
+    async addFavouriteMovie(favourite: FavouriteMovieRequest): Promise<APIResponse> {
+        const response = await this.request.post(`${this.baseUrl}/account/${this.movieAccountID}/favorite`,
             {
                 data: favourite,
                 headers: this.getAuthHeaders()
             }
         );
-        expect(response.status()).toBe(201);
-        const faveMovie: FavouriteMovieResponse = await response.json();
-        return faveMovie;
+
+        return response;
     }
 }
